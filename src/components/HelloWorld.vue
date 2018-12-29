efb215d143f93c9a9e591a75f568779e3ca1574a
unknown <1909590616@qq.com>
2018-12-29 16:54:34 +0800

    vue2websocket

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+   <websocket/>
+  </div>
+</template>
+
+<script>
+import websocket from "@/components/websocket"
+export default {
+  name: 'HelloWorld',
+  components:{
+    websocket
+  },
+  data () {
+    return {
+     
+    }
+  },
+  mounted(){
+     this.$axios
+        .post("http://112.64.170.158:9117/Service1.svc/HistoryThermalData")
+        .then(res => {
+        console.log(res)
+        })
+        .catch(error => {
+          console.log(error);
+        });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
